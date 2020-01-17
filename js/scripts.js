f51c368c7d466444b0bbe3e086b7f0ffeaacf38d
Sudhir Deolalikar <sudhir.deolalikar@vuclip.com>
2020-01-17 11:09:38 +0530

    index page

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+const bodyClass = document.querySelector('body');
+function openMenu(){
+  bodyClass.classList.add('st-openMenu');
+}
+function closeMenu(){
+  bodyClass.classList.remove('st-openMenu');
+}
