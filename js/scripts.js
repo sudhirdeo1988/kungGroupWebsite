6caeea0a9b074f95883c257537159a758f426082
Sudhir Deolalikar <sudhir.deolalikar@vuclip.com>
2020-01-24 11:41:25 +0530

    sidemenu

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
 const bodyClass = document.querySelector('body');
+const subMenuClass = document.getElementsByClassName('hasChildmenu')
 function openMenu(){
   bodyClass.classList.add('st-openMenu');
 }
 function closeMenu(){
   bodyClass.classList.remove('st-openMenu');
+  subMenuClass.classList.remove('m-open');
 }
+$(document).ready(function() {
+  $('.hasChildmenu .c-link').on('click',function(){
+    $(this).parent('.hasChildmenu').addClass('m-open');
+  });
+
+  $('.hasChildmenu .subMenu_back').on('click',function(){
+    $(this).parents('.hasChildmenu').removeClass('m-open');
+  });
+
+});
